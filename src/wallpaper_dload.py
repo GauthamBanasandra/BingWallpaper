3e43ae333454469c1738315a73d89ba41ff033b6
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-02-24 07:07:13 +0530

    Stabilized the script. Added download location.

--- src/wallpaper_dload.py
+++ src/wallpaper_dload.py
@@ -2,16 +2,22 @@ import os
 
 from selenium import webdriver
 import selenium.webdriver.support.expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 from selenium.webdriver import ActionChains
 
-# Path to chromedriver.
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import ui
 
+# Path to chromedriver.
 chromedriver_path = 'C:\Users\Gautham B A\Documents\Projects\Github\BingWallpaper\deps\chromedriver.exe'
-os.environ["webdriver.chrome.driver"] = chromedriver_path
-driver = webdriver.Chrome(chromedriver_path)
+# Set the download path (May not be necessary).
+chrome_options = webdriver.ChromeOptions()
+preferences = {'download.default_directory': 'C:\Users\Gautham B A\Documents\Projects\Github\BingWallpaper\wallpapers'}
+chrome_options.add_experimental_option('prefs', preferences)
+
+# Chrome web driver.
+driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=chrome_options)
 driver.maximize_window()
 
 # Open the webpage.
@@ -19,18 +25,23 @@ driver.get('http://www.bing.com/gallery/')
 for i in range(1, 11):
     try:
         # Image on the page.
-        img = driver.find_element_by_xpath('//*[@id="grid"]/div[' + str(i) + ']')
-        # Click the image.
-        ActionChains(driver).click(img).perform()
-        ui.WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="detailInner"]/div/div[2]/div[2]/div/div[4]/a[2]')))
-        # Download button.
-        dload_button = driver.find_element_by_xpath('//*[@id="detailInner"]/div/div[2]/div[2]/div/div[4]/a[2]')
-        # Click the download button.
-        ActionChains(driver).click(dload_button).perform()
-        # Close button
-        close_button = driver.find_element_by_xpath('//*[@id="detailInner"]/div/div[3]')
-        # Click the close button.
-        ActionChains(driver).click(close_button).perform()
+        div = driver.find_element_by_xpath('//*[@id="grid"]/div[' + str(i) + ']')
+        img = driver.find_element_by_xpath('//*[@id="grid"]/div[' + str(i) + ']/img')
+        if div is not None or img is not None:
+            src = img.get_attribute('src')
+            # Click the image.
+            ActionChains(driver).click(img).perform()
+            ui.WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="detailInner"]/div/div[2]/div[2]/div/div[4]/a[2]')))
+            # Download button.
+            dload_button = driver.find_element_by_xpath('//*[@id="detailInner"]/div/div[2]/div[2]/div/div[4]/a[2]')
+            # Click the download button.
+            ActionChains(driver).click(dload_button).perform()
+            print 'downloaded', src
     except TimeoutException:
-        print 'error', img.get_attribute('src')
+        print 'unavailable for download', src
+    except StaleElementReferenceException:
+        print 'no download button'
+    finally:
+        if div is not None:
+            div.send_keys(Keys.ESCAPE)
